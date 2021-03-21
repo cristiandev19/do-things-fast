f233b6a828d4f4b738425b79df7cfcc23519168c
cristiandev <cristiansotomayor.dev@gmail.com>
2021-03-20 23:19:34 -0500

    task card

--- src/components/TaskCard.jsx
+++ src/components/TaskCard.jsx
@@ -1,9 +1,14 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
 import useForm from '../hooks/useForm';
+import './TaskCard.css';
+import '../styles/utils.css';
+import { taskActions } from '../actions/task.actions';
 
-const TaskCard = ({ day, dayLabel, taskList = [] }) => {
-  const [tasks, setTasks] = useState(taskList);
+const TaskCard = ({
+  day, dayLabel, handleEvents, taskList = [],
+}) => {
+  // const [tasks, setTasks] = useState(taskList);
   const [formValues, handleFormChanges] = useForm({
     task: '',
   });
@@ -11,27 +16,42 @@ const TaskCard = ({ day, dayLabel, taskList = [] }) => {
   const { task } = formValues;
 
   useEffect(() => {
-    console.log('se hizo un cambio en task', task);
+    // console.log('se hizo un cambio en task', task);
   }, [task]);
 
   const handleAddTask = (event) => {
     event.preventDefault();
-    console.log('task', task);
     const timeStamp = Date.now();
-    setTasks([
-      ...tasks,
-      {
-        task,
-        id: timeStamp,
+    // setTasks([
+    //   ...tasks,
+    //   {
+    //     task,
+    //     id: timeStamp,
+    //   },
+    // ]);
+    handleEvents({
+      type    : taskActions.create,
+      payload : {
+        task: { task, id: `${timeStamp}` },
+        dayLabel,
       },
-    ]);
-    console.log('tasks', tasks);
+    });
   };
+  const handleSendAnotherList = (e, taskItem) => {
+    handleEvents({
+      type    : taskActions.move,
+      payload : {
+        task: { ...taskItem },
+        dayLabel,
+      },
+    });
+  };
+
   return (
-    <>
+    <div className="task-card">
       <h1>{dayLabel}</h1>
       <h1>{day}</h1>
-      <form onSubmit={handleAddTask}>
+      <form onSubmit={handleAddTask} className="w-100">
         <label htmlFor="newTask">
           Nueva tarea:
           <input
@@ -50,21 +70,29 @@ const TaskCard = ({ day, dayLabel, taskList = [] }) => {
       <div>
         <ul>
           {
-            tasks.map((taskItem) => <li key={taskItem.id}>{taskItem.task}</li>)
+            taskList.map((taskItem) => (
+              <li key={taskItem.id}>
+                <span>
+                  {taskItem.task}
+                </span>
+                <button type="button" onClick={(e) => handleSendAnotherList(e, taskItem)}>Enviar a la otra lista</button>
+              </li>
+            ))
           }
         </ul>
       </div>
-    </>
+    </div>
   );
 };
 
 TaskCard.propTypes = {
-  day: PropTypes.string.isRequired,
-  dayLabel: PropTypes.string.isRequired,
-  taskList: PropTypes.arrayOf(
+  day          : PropTypes.string.isRequired,
+  dayLabel     : PropTypes.string.isRequired,
+  handleEvents : PropTypes.func.isRequired,
+  taskList     : PropTypes.arrayOf(
     PropTypes.shape({
-      id: PropTypes.string.isRequired,
-      task: PropTypes.string.isRequired,
+      id   : PropTypes.string.isRequired,
+      task : PropTypes.string.isRequired,
     }),
   ).isRequired,
 };
